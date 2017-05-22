761ba1ee20ce47c5447ac6943d2d180397d1204c
bi119aTe5hXk <billgateshxk@gmail.com>
2017-05-22 09:01:09 +0900

    Fixing bugs

--- bangumi/ProgressListViewController.m
+++ bangumi/ProgressListViewController.m
@@ -28,19 +28,26 @@
 }
 
 -(void)viewWillAppear:(BOOL)animated{
-    userdefaults = [[NSUserDefaults alloc] initWithSuiteName:groupName];
-    bgmapi = [[BGMAPI alloc] initWithdelegate:self];
+    
+    
+}
+-(void)getList{
     [bgmapi getProgressListWithUID:[userdefaults stringForKey:@"userid"] WithSubID:self.subid];
     request_type = @"getProgressList";
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    
-    //NSLog(@"list:%@",self.progresslist);
-    //[self.tableView reloadData];
 }
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     self.title = @"观看进度";
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc]init];
+    [refreshControl addTarget:self action:@selector(onRefresh:) forControlEvents:UIControlEventValueChanged];
+    self.refreshControl = refreshControl;
+    userdefaults = [[NSUserDefaults alloc] initWithSuiteName:groupName];
+    bgmapi = [[BGMAPI alloc] initWithdelegate:self];
+    //NSLog(@"list:%@",self.progresslist);
+    //[self.tableView reloadData];
+    [self getList];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -48,7 +55,9 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
 }
-
+- (void)onRefresh:(id)sender{
+    [self getList];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -58,6 +67,7 @@
 -(void)api:(BGMAPI *)api readyWithList:(NSArray *)list{
     [MBProgressHUD hideHUDForView:self.view animated:YES];
     [MBProgressHUD hideHUDForView:self.view animated:YES];
+    [self.refreshControl endRefreshing];
     if ([request_type  isEqualToString: @"getProgressList"]) {
         statuslist = [list valueForKey:@"eps"];
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -66,6 +76,7 @@
         
     }
     if ([request_type isEqualToString:@"updateProgress"]) {
+        
         if ([[list valueForKey:@"error"] isEqualToString:@"OK"]) {
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"已成功记录"
                                                             message:[list valueForKey:@"error"]
@@ -73,8 +84,7 @@
                                                   cancelButtonTitle:@"了解"
                                                   otherButtonTitles:nil, nil];
             [alert show];
-            [bgmapi getProgressListWithUID:[userdefaults stringForKey:@"userid"] WithSubID:self.subid];
-            request_type = @"getProgressList";
+            
         }else{
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"记录失败"
                                                             message:[list valueForKey:@"error"]
@@ -82,12 +92,15 @@
                                                   cancelButtonTitle:@"了解"
                                                   otherButtonTitles:nil, nil];
             [alert show];
+            
         }
-        
+        [self getList];
     }
+    
     
 }
 -(void)api:(BGMAPI *)api requestFailedWithError:(NSError *)error{
+    [self.refreshControl endRefreshing];
     [MBProgressHUD hideHUDForView:self.view animated:YES];
     
 }
@@ -103,6 +116,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    [self.refreshControl endRefreshing];
     [MBProgressHUD hideHUDForView:self.view animated:YES];
 //#warning Incomplete method implementation.
     // Return the number of rows in the section.
@@ -120,40 +134,88 @@
     
     // Configure the cell...
     
-    if (row <= self.progresslist.count) {
-        NSArray *progressArr = [self.progresslist objectAtIndex:row];
-        cell.titlelabel.text = [HTMLEntityDecode htmlEntityDecode:[NSString stringWithFormat:@"ep.%@ %@",[progressArr valueForKey:@"sort"],[progressArr valueForKey:@"name"]]];
-        
-    }else{
-        cell.titlelabel.text = @"";
-    }
+//    if (row <= self.progresslist.count) {
+//        
+//        
+//    }else{
+//        cell.titlelabel.text = @"";
+//    }
     
-    if (row < statuslist.count) {
-        NSArray *progressStatuesArr = [statuslist objectAtIndex:row];
-        if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:1]) {
-            cell.sublabel.text = @"想看";
-            [cell.sublabel setTextColor:[UIColor redColor]];
-        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:2]){
-            cell.sublabel.text = @"看过";
-            [cell.sublabel setTextColor:[UIColor blueColor]];
-        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:3]){
-            cell.sublabel.text = @"在看";
-            [cell.sublabel setTextColor:[UIColor greenColor]];
-        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:4]){
-            cell.sublabel.text = @"搁置";
-            [cell.sublabel setTextColor:[UIColor grayColor]];
-        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:5]){
-            cell.sublabel.text = @"抛弃";
-            [cell.sublabel setTextColor:[UIColor darkGrayColor]];
+    NSArray *progressArr = [self.progresslist objectAtIndex:row];
+    
+    
+    for(int i = 0; i < statuslist.count; i++){
+        NSArray *progressStatuesArr = [statuslist objectAtIndex:i];
+        
+        if ([[progressStatuesArr valueForKey:@"id"] isEqual:[progressArr valueForKey:@"id"]]) {
+            //NSLog(@"id1:%@,id2:%@",[progressStatuesArr valueForKey:@"id"],[progressArr valueForKey:@"id"]);
+            
+            NSString *name_cn = [[progressStatuesArr valueForKey:@"status"] valueForKey:@"cn_name"];
+            
+            if ([name_cn  isEqualToString: @"想看"]) {
+                [cell.sublabel setTextColor:[UIColor redColor]];
+            }else if ([name_cn  isEqualToString: @"看过"]){
+                [cell.sublabel setTextColor:[UIColor blueColor]];
+            }else if ([name_cn  isEqualToString: @"在看"]){
+                [cell.sublabel setTextColor:[UIColor greenColor]];
+            }else if ([name_cn  isEqualToString: @"搁置"]){
+                [cell.sublabel setTextColor:[UIColor grayColor]];
+            }else if ([name_cn  isEqualToString: @"抛弃"]){
+                [cell.sublabel setTextColor:[UIColor darkGrayColor]];
+            }
+            cell.sublabel.text = name_cn;
+            NSLog(@"wtf:%@",name_cn);
         }else{
             cell.sublabel.text = @"";
             [cell.sublabel setTextColor:[UIColor grayColor]];
         }
-    }else{
-        cell.sublabel.text = @"";
-        [cell.sublabel setTextColor:[UIColor grayColor]];
+        
     }
     
+    
+//    if (row < statuslist.count) {
+//        NSArray *progressStatuesArr = [statuslist objectAtIndex:row];
+//        NSString *name_cn = [[progressStatuesArr valueForKey:@"status"] valueForKey:@"cn_name"];
+//        cell.sublabel.text = name_cn;
+//        [cell.sublabel setTextColor:[UIColor grayColor]];
+//        
+//        if ([name_cn  isEqualToString: @"想看"]) {
+//            [cell.sublabel setTextColor:[UIColor redColor]];
+//        }else if ([name_cn  isEqualToString: @"看过"]){
+//            [cell.sublabel setTextColor:[UIColor blueColor]];
+//        }else if ([name_cn  isEqualToString: @"在看"]){
+//            [cell.sublabel setTextColor:[UIColor greenColor]];
+//        }else if ([name_cn  isEqualToString: @"搁置"]){
+//            [cell.sublabel setTextColor:[UIColor grayColor]];
+//        }else if ([name_cn  isEqualToString: @"抛弃"]){
+//            [cell.sublabel setTextColor:[UIColor darkGrayColor]];
+//        }
+//        
+//        
+////        if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:1]) {
+////            cell.sublabel.text = @"想看";
+////            [cell.sublabel setTextColor:[UIColor redColor]];
+////        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:2]){
+////            cell.sublabel.text = @"看过";
+////            [cell.sublabel setTextColor:[UIColor blueColor]];
+////        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:3]){
+////            cell.sublabel.text = @"在看";
+////            [cell.sublabel setTextColor:[UIColor greenColor]];
+////        }else if ( == [NSNumber numberWithInteger:4]){
+////            cell.sublabel.text = @"搁置";
+////            [cell.sublabel setTextColor:[UIColor grayColor]];
+////        }else if ([[progressStatuesArr valueForKey:@"status"] valueForKey:@"id"] == [NSNumber numberWithInteger:5]){
+////            cell.sublabel.text = @"抛弃";
+////            [cell.sublabel setTextColor:[UIColor darkGrayColor]];
+////        }else{
+////            
+////        }
+//    }else{
+//        cell.sublabel.text = @"";
+//        [cell.sublabel setTextColor:[UIColor grayColor]];
+//    }
+    
+    cell.titlelabel.text = [HTMLEntityDecode htmlEntityDecode:[NSString stringWithFormat:@"ep.%@ %@",[progressArr valueForKey:@"sort"],[progressArr valueForKey:@"name"]]];
     
     cell.titlelabel.font = [UIFont systemFontOfSize:12.0f];
     cell.titlelabel.numberOfLines = 3;
@@ -257,6 +319,7 @@
 -(void)showWebViewWithURL:(NSString *)url{
     [MBProgressHUD hideHUDForView:self.view animated:YES];
     [MBProgressHUD hideHUDForView:self.view animated:YES];
+    [self.refreshControl endRefreshing];
     WebViewController *webview  = [self.storyboard instantiateViewControllerWithIdentifier:@"WebViewController"];
     webview.urlstr = url;
     webview.titlestr = @"条目讨论版";
