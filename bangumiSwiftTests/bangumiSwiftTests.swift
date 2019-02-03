fcd783b02a300e9d6bd22026b596e5953bb4214f
bi119aTe5hXk <billgateshxk@gmail.com>
2019-02-03 15:13:46 +0900

    Recreated Swift project

---
+++ bangumiSwiftTests/bangumiSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  bangumiSwiftTests.swift
+//  bangumiSwiftTests
+//
+//  Created by bi119aTe5hXk on 2019/02/03.
+//  Copyright © 2019 bi119aTe5hXk. All rights reserved.
+//
+
+import XCTest
+@testable import bangumiSwift
+
+class bangumiSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
